1dad3fd3e4cacd5722bcfb27268321348424d606
LudoHackathon <73178927+LudoHackathon@users.noreply.github.com>
2025-04-29 20:21:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="hw",
+    version="0.0.1",
+    description="Rakuten Homework",
+    packages=find_packages(),
+)
